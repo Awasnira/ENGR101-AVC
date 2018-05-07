3fc703886cf829eeb7fda6ebec19810366006c7c
Awasnira <39069532+Awasnira@users.noreply.github.com>
2018-05-08 10:19:39 +1200

    Add open gate function

--- main.cpp
+++ main.cpp
@@ -16,6 +16,19 @@ int stop(){
   set_motor(2,0);
 }
 
+void openGate() {
+	connect_to_server("", 1000); //Establish a connection to the server
+	send_to_server("please"); //Request the password from the server
+	
+	char[24] response;
+	recieve_from_server(response); //Store the server response
+	
+	send_to_server(response); //Transmit the response back to the server
+	
+	//Gate is now open.
+  
+}
+
 int main(){
   init();
   forward(50);
